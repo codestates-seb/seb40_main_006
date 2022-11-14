d16b1d8e6baffb1d30bc67817de3d2651bd8cef1
jwyong4 <jwyong4@gmail.com>
2022-11-14 13:23:52 +0900

    #002 feat: 공통 클래스 추가

---
+++ server/src/main/java/com/jamit/exception/ExceptionCode.java
@@ -0,0 +1,25 @@
+package com.jamit.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EXISTS(409, "Member exists"),
+    COFFEE_NOT_FOUND(404, "Coffee not found"),
+    COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
+    ORDER_NOT_FOUND(404, "Order not found"),
+    CANNOT_CHANGE_ORDER(403, "Order can not change"),
+    NOT_IMPLEMENTATION(501, "Not Implementation"),
+    INVALID_MEMBER_STATUS(400, "Invalid member status");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
