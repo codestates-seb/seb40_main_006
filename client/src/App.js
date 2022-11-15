5b907d53420b31d362c4cdcd0f913ad954b727fd
jaehoon <halmokme@gmail.com>
2022-11-15 18:08:17 +0900

    #7 feat: 로그인, 회원가입 페이지 ui 작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { css } from '@emotion/css';
-import palette from './Styles/theme';
 import './Styles/App.css';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 
 function App() {
-  const color = palette.colorAccent;
   return (
-    <div
-      className={css`
-        padding: 32px;
-        background-color: hotpink;
-        font-size: 24px;
-        border-radius: 4px;
-        &:hover {
-          color: ${color};
-        }
-      `}
-    >
-      Hover to change color.
+    <div>
+      <Login />
+      <Signup />
     </div>
   );
 }
---
+++ client/src/Pages/Login.js
@@ -0,0 +1,169 @@
+import * as React from 'react';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Paper,
+  Box,
+  Grid,
+  Typography,
+  Link,
+} from '@mui/material/';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { FcGoogle } from 'react-icons/fc';
+import { css } from '@emotion/css';
+import palette from '../Styles/theme';
+
+const theme = createTheme({
+  typography: {
+    fontSize: 13,
+  },
+  palette: {
+    text: {
+      primary: '#455d7a',
+    },
+    primary: {
+      main: palette.colorJamRealtime,
+      dark: palette.colorMain,
+    },
+  },
+  components: {
+    MuiInputLabel: {
+      defaultProps: {
+        sx: {
+          fontSize: '15px',
+        },
+      },
+    },
+  },
+});
+
+const socialLogin = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 38px;
+  padding: 10px;
+  margin: 20px 0 10px 0;
+  background-color: white;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  font-size: 14px;
+  cursor: pointer;
+  :hover {
+    background: hsl(210, 8%, 97.5%);
+    outline: 2px solid #e0e0e0;
+  }
+  * {
+    margin: 0 10px;
+  }
+`;
+
+export default function SignInSide() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: t =>
+              t.palette.mode === 'light'
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: palette.colorJamRealtime }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              로그인
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <button type="submit" className={socialLogin}>
+                <FcGoogle size={18} />
+                Log in with Google
+              </button>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="이메일"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                size="small"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="비밀번호"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                size="small"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                로그인
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="/" variant="body2">
+                    비밀번호를 잊으셨나요?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/" variant="body2">
+                    회원가입
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
