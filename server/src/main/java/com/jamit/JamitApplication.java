0970e729b4f12fd019cecc22708fabd9234946a9
MinaRoh <00minha@gmail.com>
2022-11-15 22:23:53 +0900

    Revert "Merge pull request #3 from codestates-seb/dev-be-global"
    This reverts commit 99b02f8e4880357581f2df1d80a63b4454e95913, reversing
    changes made to f69ce4d8ccc2f9277a97e7777d7affdd8ad7eab5.

--- server/src/main/java/com/jamit/JamitApplication.java
+++ server/src/main/java/com/jamit/JamitApplication.java
@@ -2,9 +2,7 @@ package com.jamit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class JamitApplication {
 
