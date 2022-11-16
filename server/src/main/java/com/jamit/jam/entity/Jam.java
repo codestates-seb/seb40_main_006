e57f51e91383631e687f750f71b8767a0e1e9aee
kexxxon <dev.kexon@gmail.com>
2022-11-16 13:38:41 +0900

    #15 feat: Jam entity 편의메서드 구현

--- server/src/main/java/com/jamit/jam/entity/Jam.java
+++ server/src/main/java/com/jamit/jam/entity/Jam.java
@@ -1,5 +1,7 @@
 package com.jamit.jam.entity;
 
+import static javax.persistence.FetchType.LAZY;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,53 +22,72 @@ import com.jamit.comment.entity.Comment;
 import com.jamit.global.audit.Auditable;
 import com.jamit.member.entity.Member;
 import com.jamit.reply.entity.Reply;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Jam extends Auditable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "jam_id")
-	private Long id;
 
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private Category category;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "jam_id")
+    private Long id;
 
-	@Column(nullable = false)
-	private String title;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
 
-	@Column(nullable = false)
-	private String content;
+    @Column(nullable = false)
+    private String title;
 
-	@Column(nullable = false)
-	private LocalDateTime jamFrom;
+    @Column(nullable = false)
+    private String content;
 
-	@Column(nullable = false)
-	private LocalDateTime jamTo;
+    @Column(nullable = false)
+    private LocalDateTime jamFrom;
 
-	private int currentPpl = 1;
+    @Column(nullable = false)
+    private LocalDateTime jamTo;
 
-	@Column(nullable = false)
-	private int maximum;
+    private int currentPpl = 1;
 
-	@Column(nullable = false)
-	private int capacity;
+    @Column(nullable = false)
+    private int maximum;
 
-	@Column(nullable = false)
-	private boolean realTime;
+    @Column(nullable = false)
+    private int capacity;
 
-	@Column(nullable = false)
-	private boolean complete = false;
+    @Column(nullable = false)
+    private boolean realTime;
 
-	private int views = 0;
+    @Column(nullable = false)
+    private boolean complete = false;
 
-	@ManyToOne
-	@JoinColumn(name = "member_id")
-	private Member member;
+    private int views = 0;
 
-	@OneToMany(mappedBy = "jam", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-	private final List<Comment> commentList = new ArrayList<>();
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
-	@OneToMany(mappedBy = "jam", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-	private final List<Reply> replyList = new ArrayList<>();
+    @OneToMany(mappedBy = "jam", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private final List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "jam", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private final List<Reply> replyList = new ArrayList<>();
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void addComment(Comment comment) {
+        commentList.add(comment);
+    }
+
+    public void addReply(Reply reply) {
+        replyList.add(reply);
+    }
 }
