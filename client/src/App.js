ac39e7f74649b122292c1278d2d0dd13138778db
MinaRoh <00minha@gmail.com>
2022-11-16 11:49:48 +0900

    #10 style: 사이드바 기본 디자인

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 // import palette from './Styles/theme';
 import './Styles/App.css';
 import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   // const color = palette.colorAccent;
-  return <Header />;
+  return (
+    <>
+      <Header />;
+      <Sidebar />;
+    </>
+  );
 }
 
 export default App;
