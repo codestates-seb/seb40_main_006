2c00bc39f0903fe97882fc450e9ac1116fce647a
MinaRoh <00minha@gmail.com>
2022-11-17 10:08:13 +0900

    #10 style: 헤더 검색창 디자인 수정

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,10 +1,7 @@
 import { css } from '@emotion/css';
-// import TextField from '@mui/material/TextField';
-// import Paper from '@mui/material/Paper';
-// import InputBase from '@mui/material/InputBase';
-// import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
-// import { useState } from 'react';
+import InputBase from '@mui/material/InputBase';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import palette from '../Styles/theme';
 import logoImage from '../Assets/images/logo_header.png';
@@ -15,10 +12,12 @@ const header = css`
   background-color: white;
   width: 100%;
   height: 100px;
-  border-bottom: 0.5px gray solid;
+  border-bottom: 0.5px ${palette.border} solid;
 `;
 // logo
-const logo = css``;
+const logo = css`
+  margin-left: 10px;
+`;
 // address
 const addressArea = css`
   display: flex;
@@ -45,13 +44,13 @@ const searchBar = css`
   margin: 15px 10px;
   flex-grow: 1;
 `;
-const searchInput = css`
-  background-color: transparent;
-  border-radius: 10px;
-  border-width: 0px;
-  width: 100%;
-  padding: 15px 10px;
-`;
+// const searchInput = css`
+//   background-color: transparent;
+//   border-radius: 10px;
+//   border-width: 0px;
+//   width: 100%;
+//   padding: 15px 10px;
+// `;
 // login
 const loginArea = css`
   display: flex;
@@ -82,26 +81,52 @@ const username = css`
   margin: 10px;
 `;
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  width: '100%',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: (theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: (theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 1),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  display: 'flex',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+}));
+
 const SearchBar = () => {
   return (
     <div className={searchBar}>
-      <SearchIcon />
-      <input
-        className={searchInput}
-        placeholder="제목이나 내용, 유저이름으로 검색해보세요!"
-      />
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="제목이나 내용, 유저이름으로 검색해보세요!"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
     </div>
-    // <Paper
-    //   component="form"
-    //   sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', grow: 1 }}
-    // >
-    //   <SearchIcon />
-    //   <InputBase
-    //     sx={{ ml: 1, flex: 1, grow: 1 }}
-    //     placeholder="제목이나 내용, 유저이름으로 검색해보세요!"
-    //     inputProps={{ 'aria-label': '검색창' }}
-    //   />
-    // </Paper>
   );
 };
 
