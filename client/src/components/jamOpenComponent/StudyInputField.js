73814451695302ee75d19be5a9abbe68f0911130
JKKIM <jkesonjk@gmail.com>
2022-11-17 09:19:36 +0900

    #005 style: 잼개설페이지 구현 및 인풋 컴포넌트 작성

---
+++ client/src/components/jamOpenComponent/StudyInputField.js
@@ -0,0 +1,140 @@
+/** @jsxImportSource @emotion/react */
+import React, { useState } from 'react';
+import { TextField, Box, MenuItem } from '@mui/material';
+import { css } from '@emotion/react';
+
+const Container = css`
+  width: 100%;
+  height: 356px;
+`;
+
+const InputContainer = css`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 25px;
+`;
+
+const categories = [
+  { value: 'health', label: '운동/건강' },
+  { value: 'lifestyle', label: '라이프스타일' },
+  { value: 'cooking', label: '요리' },
+  { value: 'art', label: '미술' },
+  { value: 'career', label: '커리어' },
+  { value: 'craft', label: '공예' },
+  { value: 'photography', label: '사진/영상' },
+  { value: 'music', label: '음악' },
+  { value: 'language', label: '외국어' },
+  { value: 'education', label: '교육' },
+  { value: 'investment', label: '재테크' },
+  { value: 'business', label: '비즈니스' },
+  { value: 'development', label: '개발' },
+];
+
+const StudyInputField = () => {
+  const [category, setCategory] = useState('health');
+
+  const handleChange = e => {
+    setCategory(e.target.value);
+  };
+
+  return (
+    <div css={Container}>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '28ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div css={InputContainer}>
+          <TextField
+            id="study-name"
+            label="스터디 이름"
+            variant="standard"
+            placeholder="스터디 이름을 지어주세요"
+            sx={{
+              width: 300,
+              maxWidth: '100%',
+              '& .MuiInputLabel-root.Mui-focused': { color: 'black' }, // 기본 라벨, 포커스시 라벨 색상
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black', // 포커스시 아래라인 색상
+              },
+            }}
+          />
+          <TextField
+            id="study-category"
+            select
+            label="카테고리"
+            variant="standard"
+            value={category}
+            defaultValue="클릭해서 선택해주세요"
+            onChange={handleChange}
+            sx={{
+              width: 300,
+              maxWidth: '100%',
+              '& .MuiInputLabel-root.Mui-focused': { color: 'black' },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black',
+              },
+            }}
+          >
+            {categories.map(option => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="study-location"
+            label="스터디 위치"
+            variant="standard"
+            placeholder="클릭하면 주소 검색창이 나와요"
+            sx={{
+              width: 300,
+              maxWidth: '100%',
+              '& .MuiInputLabel-root.Mui-focused': { color: 'black' },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black',
+              },
+            }}
+          />
+          <TextField
+            id="study-peopleNumber"
+            label="모집인원"
+            type="number"
+            variant="standard"
+            InputProps={{ inputProps: { min: 0, max: 100 } }}
+            placeholder="숫자를 입력해주세요"
+            sx={{
+              width: 300,
+              maxWidth: '100%',
+              '& .MuiInputLabel-root.Mui-focused': { color: 'black' },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black',
+              },
+            }}
+          />
+          <TextField
+            id="study-period"
+            label="모집기간"
+            variant="standard"
+            sx={{
+              width: 300,
+              maxWidth: '100%',
+              '& .MuiInputLabel-root.Mui-focused': { color: 'black' },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'black',
+              },
+            }}
+          />
+        </div>
+      </Box>
+    </div>
+  );
+};
+
+export default StudyInputField;
--- client/src/pages/JamOpen.js
+++ client/src/pages/JamOpen.js
@@ -1,83 +1,232 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState } from 'react';
 import { css } from '@emotion/react';
+import { ThemeProvider } from '@mui/material';
+import { BsPlusCircle } from 'react-icons/bs';
+import palette from '../Styles/theme';
+// import Button from '../components/Button';
+import StudyInputField from '../components/jamOpenComponent/StudyInputField';
+import JamInputField from '../components/jamOpenComponent/JamInputField';
+import Description from '../components/jamOpenComponent/Description';
+import EditButtonGroup from '../components/jamOpenComponent/EditButtonGroup';
 
 const Container = css`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   padding: 5px;
+  max-width: 800px;
 `;
 
 const Header = css`
-  background-color: hotpink;
+  width: 100%;
+  background-color: white;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
+  justify-content: center;
+  margin-bottom: 5px;
 `;
 
 const HeaderText = css`
-  font-size: 16px;
+  font-size: 15px;
+  padding: 10px 0;
+  margin-left: 5px;
 `;
 
 const HeaderTap = css`
+  width: 100%;
   font-size: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`;
+
+const OpenStudyButton = css`
+  border-radius: 5px;
+  border: none;
+  background-color: #ffcabb;
+  color: black;
+  font-weight: 500;
+  line-height: 20px;
+  height: 40px;
+  font-size: 14px;
+  padding: 5px 20px;
+  text-align: center;
+  cursor: pointer;
 `;
 
 const Tapstyle = css`
-  background-color: #dcdcdc;
-  color: rgba(73, 73, 73, 0.5);
-  font-weight: bold;
+  width: 180px;
+  height: 50px;
+  font-size: 15px;
+  text-align: center;
+  background-color: white;
+  color: black;
   display: flex;
   flex-direction: row;
   justify-items: center;
   align-items: center;
-  list-style: none;
-  margin-bottom: 7rem;
 
   .default {
     width: 100%;
-    padding: 15px 10px;
+    padding: 10px 6px;
     cursor: pointer;
+    border: 1px solid ${palette.colorBtn3};
   }
 
   .focused {
-    background-color: #ffcabb;
+    background-color: ${palette.colorMain};
     color: black;
-    transition: 0.3s;
+    /* border-bottom: 5px solid ${palette.colorBtn2}; */
+    transition: 0.2s;
+  }
+
+  button {
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+`;
+
+const FileUpload = css`
+  width: 100%;
+  height: 290px;
+  background-color: #dddddd;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const SectionContainer = css`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const ArticleLeft = css`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: space-between;
+  margin-right: 10px;
+`;
+
+const ArticleRight = css`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const Chatlink = css`
+  width: 100%;
+  height: 60px;
+  border: 1px solid #dddddd;
+  background-color: #dddddd;
+  margin-top: 10px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  input,
+  input:focus {
+    font-size: 15px;
+    border: none;
+    padding: 0 15px;
+    background-color: #dddddd;
+    width: 100%;
+    height: 90%;
+    outline: none;
   }
 `;
 
 const JamOpen = () => {
   const [currentTab, setCurrentTab] = useState(0);
+  const [isCreated, setIsCreated] = useState(false);
 
   const tapArr = [
     { id: 0, name: '스터디 잼', content: '스터디 잼 내용' },
     { id: 1, name: '실시간 잼', content: '실시간 잼 내용' },
   ];
 
-  const selectMenuHandler = el => {
+  const selectTapHandler = el => {
     setCurrentTab(el.id);
   };
 
   return (
     <div css={Container}>
-      <div css={Header}>
+      <header css={Header}>
         <div css={HeaderText}>카테고리 &gt; 요리</div>
         <div css={HeaderTap}>
-          <ul css={Tapstyle}>
-            {tapArr.map(el => (
-              <li
-                key={el.id}
-                className={currentTab === el.id ? 'default focused' : 'default'}
-                onClick={() => selectMenuHandler(el)}
-                aria-hidden="true"
-              >
-                {el.name}
-              </li>
-            ))}
-          </ul>
-          <button type="button">개설하기</button>
+          <ThemeProvider theme={palette}>
+            <div css={Tapstyle}>
+              {tapArr.map(el => (
+                <button
+                  key={el.id}
+                  type="button"
+                  className={
+                    currentTab === el.id ? 'default focused' : 'default'
+                  }
+                  onClick={() => selectTapHandler(el)}
+                  aria-hidden="true"
+                >
+                  {el.name}
+                </button>
+              ))}
+            </div>
+          </ThemeProvider>
+          {isCreated ? (
+            <EditButtonGroup
+              isCreated={isCreated}
+              setIsCreated={setIsCreated}
+            />
+          ) : (
+            <button
+              css={OpenStudyButton}
+              type="submit"
+              form="test"
+              onClick={() => setIsCreated(!isCreated)}
+            >
+              개설하기
+            </button>
+          )}
+
+          {/* <Button
+            size="md"
+            variant="default"
+            type="submit"
+            form="test"
+            // onClick={() => handleSubmit()}
+          >
+            개설하기
+          </Button> */}
         </div>
-      </div>
+      </header>
+      <main css={SectionContainer} id="test">
+        <div css={ArticleLeft}>
+          <div css={FileUpload}>
+            <BsPlusCircle size={40} />
+            <span>스터디를 표현할 이미지를 추가해주세요</span>
+          </div>
+          <div css={Chatlink}>
+            <input
+              type="text"
+              placeholder="잼 그룹원과 소통할 채팅 채널을 기재해주세요(카카오 오픈채팅 등)"
+            />
+          </div>
+        </div>
+        <div css={ArticleRight}>
+          {currentTab === 0 ? <StudyInputField /> : <JamInputField />}
+        </div>
+      </main>
+      <Description />
     </div>
   );
 };
