750048c21c7a5fda7de3947902ad0bea8e24ff5a
JulyK9 <jkesonjk@gmail.com>
2022-11-17 13:54:03 +0900

    [client] style: 잼 개설, 상세, 수정 페이지 구현
    * 잼개설페이지 헤더부분 작업중
    
    * 버튼 컴포넌트 작업 완료
    
    * #005 chore: MUI Material 설치
    
    * #005 style: 잼개설페이지 구현 및 인풋 컴포넌트 작성
    
    * #005 style: 잼수정페이지 렌더될 버튼그룹 작성
    
    * #005 chore: eslint 추가
    
    * #005 chore: eslint 관련 패키지 설치 추가
    
    * #005 style: 잼상세페이지 작성
    
    * #005 style: 잼상세페이지 잼정보 컴포넌트 작성
    
    * #005 style: 잼상세페이지 댓글 부분 컴포넌트 작성
    
    * #005 style: 잼상세페이지 댓글 부분 컴포넌트 추가 작성
    
    * #005 style: 잼상세페이지 사이드바 컴포넌트 작성
    
    * #005 style: 잼상세페이지 사이드바 모집상태 스타일 관련 컴포넌트 작성
    
    * #005 style: 공통 활용 가능한 버튼 컴포넌트 작성
    
    * #005 chore: react suite 설치
    
    * #005 style: react suite 적용

---
+++ client/src/components/jamOpenComponent/Description.js
@@ -0,0 +1,69 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+import { TextField, Box } from '@mui/material';
+
+const AboutStudy = css`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  span {
+    margin-bottom: 5px;
+  }
+  input {
+    width: 100%;
+    height: 300px;
+  }
+  input:focus {
+    outline: none;
+  }
+`;
+
+const Description = () => {
+  return (
+    <div css={AboutStudy}>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 0, width: '800px' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            id="standard-basic"
+            label="스터디 소개"
+            multiline
+            rows={15}
+            variant="outlined"
+            placeholder="함께할 스터디를 소개해주세요"
+            // 커스텀 색상 설정
+            sx={{
+              '& > :not(style)': { m: 0, width: '790px' },
+              '& label, label.Mui-focused': {
+                color: 'grey', // 기본 라벨, 포커스시 라벨 색상
+              },
+              '&.MuiOutlinedInput-root:hover': {
+                color: 'grey', // 호버시 외곽선 색상
+              },
+              '& .MuiOutlinedInput-root': {
+                '& > fieldset': { borderColor: '#d2d2d2' }, // 기본 외곽선 색상
+              },
+              '& .MuiOutlinedInput-root.Mui-focused': {
+                '& > fieldset': {
+                  borderColor: '#d2d2d2', // 포커스시 외곽선 색상
+                },
+              },
+            }}
+          />
+        </div>
+      </Box>
+    </div>
+  );
+};
+
+export default Description;
