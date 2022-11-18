e8d9b72867a83dfa7ec6df1f49e17e078375b7fb
kexxxon <dev.kexon@gmail.com>
2022-11-18 17:44:00 +0900

    #19 feat: implements patch logic in JamService

--- server/src/main/java/com/jamit/jam/service/JamService.java
+++ server/src/main/java/com/jamit/jam/service/JamService.java
@@ -1,9 +1,12 @@
 package com.jamit.jam.service;
 
+import com.jamit.exception.BusinessLogicException;
+import com.jamit.exception.ExceptionCode;
 import com.jamit.jam.entity.Jam;
 import com.jamit.jam.repository.JamRepository;
 import com.jamit.member.entity.Member;
 import com.jamit.member.repository.MemberRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,8 +29,17 @@ public class JamService {
     }
 
     public Jam updateJam(Jam jam) {
+        Jam verifiedJam = findVerifiedJam(jam.getId());
 
-        return null;
+        Optional.ofNullable(jam.getTitle()).ifPresent(verifiedJam::setTitle);
+        Optional.ofNullable(jam.getCategory()).ifPresent(verifiedJam::setCategory);
+        Optional.of(jam.getMaximum()).ifPresent(verifiedJam::setMaximum);
+        Optional.ofNullable(jam.getJamFrom()).ifPresent(verifiedJam::setJamFrom);
+        Optional.ofNullable(jam.getJamTo()).ifPresent(verifiedJam::setJamTo);
+        Optional.of(jam.isRealTime()).ifPresent(verifiedJam::setRealTime);
+        Optional.ofNullable(jam.getContent()).ifPresent(verifiedJam::setContent);
+
+        return jamRepository.save(verifiedJam);
     }
 
     public Jam findJam(Long jamId) {
@@ -37,5 +49,11 @@ public class JamService {
 
     public void deleteJam(Long jamId) {
 
+    }
+
+    public Jam findVerifiedJam(Long jamId) {
+        Optional<Jam> getJam = jamRepository.findById(jamId);
+
+        return getJam.orElseThrow(() -> new BusinessLogicException(ExceptionCode.JAM_NOT_FOUND));
     }
 }
