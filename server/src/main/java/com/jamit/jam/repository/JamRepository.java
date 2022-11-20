437a08008e1a955e9971cdc9eb00467c3f0f36bf
kexxxon <dev.kexon@gmail.com>
2022-11-20 22:28:45 +0900

    #19 feat: JamRepository

--- server/src/main/java/com/jamit/jam/repository/JamRepository.java
+++ server/src/main/java/com/jamit/jam/repository/JamRepository.java
@@ -2,10 +2,16 @@ package com.jamit.jam.repository;
 
 import com.jamit.jam.entity.Jam;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JamRepository extends JpaRepository<Jam, Long> {
     Optional<Jam> findById(Long memberId);
+
+    @Query("select jam from Jam jam order by jam.createdAt desc")
+    Page<Jam> jamPage(Pageable pageable);
 }
