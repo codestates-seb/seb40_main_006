00e123c78ee8aac17af93e7f3ac743d0f4e3912b
kexxxon <dev.kexon@gmail.com>
2022-11-23 05:12:49 +0900

    #46 feat: Comment/Reply/Jam Mapper

---
+++ server/src/main/java/com/jamit/comment/mapper/CommentMapper.java
@@ -0,0 +1,20 @@
+package com.jamit.comment.mapper;
+
+import com.jamit.comment.dto.ResponseCommentDto;
+import com.jamit.comment.entity.Comment;
+import com.jamit.reply.mapper.ReplyMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", uses = ReplyMapper.class,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CommentMapper {
+
+    @Mapping(source = "id", target = "commentId")
+    @Mapping(source = "member.memberId", target = "memberId")
+    @Mapping(source = "jam.id", target = "jamId")
+    @Mapping(source = "member.image", target = "image")
+    @Mapping(source = "member.nickname", target = "nickname")
+    ResponseCommentDto commentToResponseCommentDto(Comment comment);
+}
--- server/src/main/java/com/jamit/jam/mapper/JamMapper.java
+++ server/src/main/java/com/jamit/jam/mapper/JamMapper.java
@@ -1,5 +1,6 @@
 package com.jamit.jam.mapper;
 
+import com.jamit.comment.mapper.CommentMapper;
 import com.jamit.jam.dto.*;
 import com.jamit.jam.entity.Jam;
 import com.jamit.jam.entity.JamParticipant;
@@ -9,8 +10,10 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import java.util.List;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = CommentMapper.class,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface JamMapper {
 
     Jam jamPostDtoToJam(JamPostDto jamPostDto);
@@ -18,16 +21,16 @@ public interface JamMapper {
     Jam jamPatchDtoToJam(JamPatchDto jamPatchDto);
 
     @Named("jamList")
-    @Mapping(source = "id",target = "jamId")
-    @Mapping(source = "member.nickname",target = "nickname")
+    @Mapping(source = "id", target = "jamId")
+    @Mapping(source = "member.nickname", target = "nickname")
     ResponseAllJamsDto jamToResponseAllJamsDto(Jam jam);
 
-    @Mapping(source = "id",target = "jamId")
-    @Mapping(source = "member.nickname",target = "nickname")
+    @Mapping(source = "id", target = "jamId")
+    @Mapping(source = "member.nickname", target = "nickname")
     ResponseSpecificJamDto jamToResponseSpecificJamDto(Jam jam);
 
-    @Mapping(source = "member.memberId",target = "memberId")
-    @Mapping(source = "member.nickname",target = "nickname")
+    @Mapping(source = "member.memberId", target = "memberId")
+    @Mapping(source = "member.nickname", target = "nickname")
     ResponseParticipantDto participantToParticipantListDto(JamParticipant jamParticipant);
 
     @IterableMapping(qualifiedByName = "jamList")
