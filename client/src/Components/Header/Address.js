a91eff93ed84f08ad49f220edc88113454ff721e
MinaRoh <00minha@gmail.com>
2022-11-23 14:49:14 +0900

    #29 feat: 중첩메뉴 전체 구현

--- client/src/Components/Header/Address.js
+++ client/src/Components/Header/Address.js
@@ -30,29 +30,50 @@ const NestedMenu = () => {
   const handleClose = () => setAnchorEl(null);
 
   const [cityList, setCityList] = useState([]);
-  function getData(code) {
+  const [guList, setGuList] = useState([]);
+  const [dongList, setDongList] = useState([]);
+
+  // 대한민국의 모든 특별/광역시, 도 반환
+  function getCityData(code) {
     const apiLocation = getJuso(code);
     apiLocation.then(data => {
-      setCityList(data.regcodes.map(el => el.name));
+      setCityList(data.regcodes);
     });
-    // .then(codes => {
-    // console.log(si);
-    // si.forEach(el => setSi([...el.name]));
-    // console.log(codes);
-    // });
+  }
+  // 선택한 시(cityCode) 소속의 모든 구 반환
+  function getGuData(cityCode) {
+    const code = cityCode.slice(0, 2).concat('*000000');
+    const apiLocation = getJuso(code);
+    apiLocation.then(data => {
+      setGuList(data.regcodes.slice(1));
+    });
+  }
+  // 선택한 구(guCode)의 모든 동 반환
+  function getDongData(guCode) {
+    const code = guCode.slice(0, 4).concat('*&is_ignore_zero=true');
+    const apiLocation = getJuso(code);
+    apiLocation.then(data => {
+      setDongList(data.regcodes);
+    });
   }
 
   useEffect(() => {
     // '*00000000' 행정시
     // '11*000000' 서울 내 구 41 경기도
     // 1153*&is_ignore_zero=true
-    getData('*00000000');
+    /*
+    '*00000000', // 대한민국의 모든 특별/광역시, 도 반환
+    '11*', // 서울시 소속 모든 구, 동 반환
+    '11*000000', // 서울시 소속 모든 구 반환
+    '1153*', // 구로구를 포함한 구로구의 모든 동 반환
+    '1153*&is_ignore_zero=true', // 구로구의 모든 동만 반환
+    */
+    getCityData('*00000000');
   }, []);
 
   useEffect(() => {
-    console.log(cityList);
-  }, [cityList]);
-
+    // console.log(dongList);
+  }, [dongList]);
   return (
     <div className={addressContainer}>
       <button className={addressBtn} onClick={handleClick} type="button">
@@ -60,8 +81,24 @@ const NestedMenu = () => {
       </button>
       <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
         {cityList.map(el => (
-          <NestedMenuItem label={el} parentMenuOpen={open}>
-            <MenuItem onClick={handleClose}>{el}</MenuItem>
+          <NestedMenuItem
+            label={el.name}
+            parentMenuOpen={open}
+            onMouseOver={() => getGuData(el.code)}
+          >
+            {guList.map(gu => (
+              <NestedMenuItem
+                label={gu.name.split(' ')[1]}
+                parentMenuOpen={open}
+                onMouseOver={() => getDongData(gu.code)}
+              >
+                {dongList.map(dong => (
+                  <MenuItem onClick={handleClose}>
+                    {dong.name.split(' ')[2]}
+                  </MenuItem>
+                ))}
+              </NestedMenuItem>
+            ))}
           </NestedMenuItem>
         ))}
         <NestedMenuItem label="Top Level" parentMenuOpen={open}>
