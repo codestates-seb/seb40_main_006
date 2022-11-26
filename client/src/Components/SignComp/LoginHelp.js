bba154b4673fcd98c77bdbe549a4e518ce0d012d
jaehoon <halmokme@gmail.com>
2022-11-26 18:24:55 +0900

    #76 feat: 로그인 상태시 로그인 페이지 접근 불가

--- client/src/Components/SignComp/LoginHelp.js
+++ client/src/Components/SignComp/LoginHelp.js
@@ -10,7 +10,7 @@ const LoginHelp = () => {
         </Link>
       </Grid>
       <Grid item>
-        <Link href="/" variant="body2">
+        <Link href="/signup" variant="body2">
           회원가입
         </Link>
       </Grid>
