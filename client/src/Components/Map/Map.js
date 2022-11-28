4ba38ea80e5978f7821b9eda29e605b63b7f400e
MinaRoh <00minha@gmail.com>
2022-11-28 14:59:35 +0900

    #72 feat: kakaoMap 이동시 중심 좌표 가져오기

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -4,23 +4,22 @@ import { location } from '../../Atom/atoms';
 
 const { kakao } = window;
 const Map = () => {
-  const [latitude, setLatitude] = useState(37.5602098);
-  const [longitude, setLongitude] = useState(126.825479);
+  const [latitude] = useState(37.5602098);
+  const [longitude] = useState(126.825479);
 
   // 장소 검색 객체를 생성합니다
   const ps = new kakao.maps.services.Places();
   const [currentLocation] = useRecoilState(location);
   // 좌표로 주소 얻기
   function getMap() {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-
-      // console.log(`위도 : ${position.coords.latitude}`);
-      // console.log(`경도 : ${position.coords.longitude}`);
-      // console.log(latitude);
-      // console.log(longitude);
-    });
+    // navigator.geolocation.getCurrentPosition(function (position) {
+    //   setLatitude(position.coords.latitude);
+    //   setLongitude(position.coords.longitude);
+    // console.log(`위도 : ${position.coords.latitude}`);
+    // console.log(`경도 : ${position.coords.longitude}`);
+    // console.log(latitude);
+    // console.log(longitude);
+    // });
 
     const container = document.getElementById('map'); // 지도를 표시할 div
     const options = {
@@ -28,11 +27,21 @@ const Map = () => {
       level: 3, // 지도 확대 레벨
     };
 
-    // eslint-disable-next-line no-unused-vars
     const map = new kakao.maps.Map(container, options);
+    // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'center_changed', function () {
+      // 지도의  레벨을 얻어옵니다
+      const level = map.getLevel();
 
+      // 지도의 중심좌표를 얻어옵니다
+      const latlng = map.getCenter();
+
+      const message = `지도 레벨은 ${level} 이고 중심 좌표는 위도 ${latlng.getLat()}, 경도 ${latlng.getLng()}입니다`;
+      console.log(message);
+    });
     return map;
   }
+
   useEffect(() => {
     console.log(currentLocation);
     getMap();
@@ -57,6 +66,7 @@ const Map = () => {
 
   // 키워드로 장소를 검색합니다
   ps.keywordSearch(currentLocation, placesSearchCB);
+
   return (
     <div
       id="map"
