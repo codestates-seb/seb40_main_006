e1fd47e3b1a80294f7b8e9ebb0a5d389f4066718
MinaRoh <00minha@gmail.com>
2022-11-28 18:32:06 +0900

    #72 feat: 더미데이터로 마커 표시 추가

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -2,25 +2,22 @@ import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { location } from '../../Atom/atoms';
 
+// 33.450701, 126.570667
 const { kakao } = window;
 const Map = () => {
-  const [latitude] = useState(37.5602098);
-  const [longitude] = useState(126.825479);
+  // 마곡
+  // const [latitude] = useState(37.5602098);
+  // const [longitude] = useState(126.825479);
+
+  // 제주
+  const [latitude] = useState(33.450701);
+  const [longitude] = useState(126.570667);
 
   // 장소 검색 객체를 생성합니다
-  const ps = new kakao.maps.services.Places();
+  // const ps = new kakao.maps.services.Places();
   const [currentLocation] = useRecoilState(location);
   // 좌표로 주소 얻기
   function getMap() {
-    // navigator.geolocation.getCurrentPosition(function (position) {
-    //   setLatitude(position.coords.latitude);
-    //   setLongitude(position.coords.longitude);
-    // console.log(`위도 : ${position.coords.latitude}`);
-    // console.log(`경도 : ${position.coords.longitude}`);
-    // console.log(latitude);
-    // console.log(longitude);
-    // });
-
     const container = document.getElementById('map'); // 지도를 표시할 div
     const options = {
       center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
@@ -42,30 +39,80 @@ const Map = () => {
     return map;
   }
 
-  useEffect(() => {
-    console.log(currentLocation);
-    getMap();
-  }, [currentLocation]);
-
   // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-  function placesSearchCB(data, status) {
-    if (status === kakao.maps.services.Status.OK) {
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-      // LatLngBounds 객체에 좌표를 추가합니다
-      const bounds = new kakao.maps.LatLngBounds();
+  // function placesSearchCB(data, status) {
+  //   if (status === kakao.maps.services.Status.OK) {
+  //     // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+  //     // LatLngBounds 객체에 좌표를 추가합니다
+  //     const bounds = new kakao.maps.LatLngBounds();
 
-      for (let i = 0; i < data.length; i += 1) {
-        // displayMarker(data[i]);
-        bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-      }
+  //     for (let i = 0; i < data.length; i += 1) {
+  //       // displayMarker(data[i]);
+  //       bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+  //     }
 
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-      getMap().setBounds(bounds);
-    }
-  }
+  //     // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+  //     getMap().setBounds(bounds);
+  //   }
+  // }
 
   // 키워드로 장소를 검색합니다
-  ps.keywordSearch(currentLocation, placesSearchCB);
+  // ps.keywordSearch(currentLocation, placesSearchCB);
+
+  const MapPin = () => {
+    // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
+    const positions = [
+      {
+        content: '<div>마곡</div>',
+        latlng: new kakao.maps.LatLng(37.5602098, 126.825479),
+      },
+      {
+        content: '<div>생태연못</div>',
+        latlng: new kakao.maps.LatLng(33.450936, 126.569477),
+      },
+      {
+        content: '<div>텃밭</div>',
+        latlng: new kakao.maps.LatLng(33.450879, 126.56994),
+      },
+      {
+        content: '<div>근린공원</div>',
+        latlng: new kakao.maps.LatLng(33.451393, 126.570738),
+      },
+    ];
+    const map = getMap();
+    for (let i = 0; i < positions.length; i += 1) {
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커의 위치
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다
+      const infowindow = new kakao.maps.InfoWindow({
+        content: positions[i].content, // 인포윈도우에 표시할 내용
+      });
+
+      // 마커에 이벤트를 등록하는 함수 만들고 즉시 호출하여 클로저를 만듭니다
+      // 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+      (function (point, window) {
+        // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시합니다
+        kakao.maps.event.addListener(point, 'mouseover', function () {
+          window.open(map, point);
+        });
+
+        // 마커에 mouseout 이벤트를 등록하고 마우스 아웃 시 인포윈도우를 닫습니다
+        kakao.maps.event.addListener(point, 'mouseout', function () {
+          window.close();
+        });
+      })(marker, infowindow);
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentLocation);
+    // getMap();
+    MapPin();
+  }, [currentLocation]);
 
   return (
     <div
