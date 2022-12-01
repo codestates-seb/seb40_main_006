ce5ec82c90db9f40d9942fa0e70f861860e2d47d
kexxxon <dev.kexon@gmail.com>
2022-12-01 17:31:21 +0900

    #90 feat: JamRepository

--- server/src/main/java/com/jamit/jam/repository/JamRepository.java
+++ server/src/main/java/com/jamit/jam/repository/JamRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -31,4 +32,8 @@ public interface JamRepository extends JpaRepository<Jam, Long> {
     // 작성자 검색
     @Query(value = "SELECT * FROM jam WHERE member_id = :memberId ORDER BY created_At DESC", nativeQuery = true)
     List<Jam> findByJamMemberId(@Param("memberId") Long memberId);
+
+    @Modifying
+    @Query("update Jam jam set jam.views = jam.views + 1 where jam.id = :id")
+    int updateView(Long id);
 }
