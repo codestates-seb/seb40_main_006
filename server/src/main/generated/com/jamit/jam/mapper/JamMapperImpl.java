911964f4184cf7d9faaf630503aadd253a05d0ab
jylee-me <jylee9432@gmail.com>
2022-12-01 20:53:34 +0900

    #124 refactoring

--- server/src/main/generated/com/jamit/jam/mapper/JamMapperImpl.java
+++ server/src/main/generated/com/jamit/jam/mapper/JamMapperImpl.java
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-01T17:54:05+0900",
+    date = "2022-12-01T20:36:15+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.15 (Azul Systems, Inc.)"
 )
 @Component
