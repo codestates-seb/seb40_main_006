b09d222725173ec527f0784298b787273e3b472b
MinaRoh <00minha@gmail.com>
2022-12-03 11:21:21 +0900

    #144 fix: 지도에 마커가 보이지 않는 오류 수정
    - 지도를 움직여야만 마커가 보이는 오류가 있습니다.

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -1,23 +1,32 @@
+/* eslint-disable no-loop-func */
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { coordinate, location } from '../../Atom/atoms';
+// import { useRecoilState } from 'recoil';
+// import { location } from '../../Atom/atoms';
 
-// 33.450701, 126.570667
 const { kakao } = window;
 const Map = ({ jamData }) => {
   // 마곡
-  const [latitude] = useState(37.5602098);
-  const [longitude] = useState(126.825479);
-
-  // 제주
-  // const [latitude] = useState(33.450701);
-  // const [longitude] = useState(126.570667);
+  const [latitude, setLatitude] = useState(37.5602098);
+  const [longitude, setLongitude] = useState(126.825479);
 
   // 장소 검색 객체를 생성합니다
   // const ps = new kakao.maps.services.Places();
-  const [currentLocation] = useRecoilState(location);
-  const [currentCoordinate, setCurrentCoordinate] = useRecoilState(coordinate);
+  // const [currentLocation] = useRecoilState(location); // 스타벅스 마곡역점
+  // const [currentCoordinate] = useRecoilState(coordinate); // 37.5602098, 126.825479
+  // setLatitude(currentCoordinate.latitude);
+  // setLongitude(currentCoordinate.longitude);
+
+  // 초기 설정
+  // const con = document.getElementById('map'); // 지도를 표시할 div
+  // const op = {
+  //   center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
+  //   level: 4, // 지도 확대 레벨
+  // };
+
+  // // eslint-disable-next-line no-unused-vars
+  // let map = new kakao.maps.Map(con, op);
+
   // 좌표로 주소 얻기
   function getMap() {
     const container = document.getElementById('map'); // 지도를 표시할 div
@@ -27,20 +36,13 @@ const Map = ({ jamData }) => {
     };
 
     const map = new kakao.maps.Map(container, options);
+
     // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
     kakao.maps.event.addListener(map, 'center_changed', function () {
-      // 지도의  레벨을 얻어옵니다
-      // const level = map.getLevel();
-
       // 지도의 중심좌표를 얻어옵니다
       const latlng = map.getCenter();
-      setCurrentCoordinate({
-        latitude: latlng.getLat(),
-        longitude: latlng.getLng(),
-      });
-
-      // const message = `지도 레벨은 ${level} 이고 중심 좌표는 위도 ${latlng.getLat()}, 경도 ${latlng.getLng()}입니다`;
-      // console.log(message);
+      setLatitude(latlng.getLat());
+      setLongitude(latlng.getLng());
     });
     return map;
   }
@@ -66,15 +68,17 @@ const Map = ({ jamData }) => {
   // ps.keywordSearch(currentLocation, placesSearchCB);
 
   const MapPin = () => {
+    console.log('MapPin 진입했습니다.');
     // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
     const mapPoints = [];
-    const map = getMap();
+
     jamData.forEach(jam => {
       mapPoints.push({
         content: `<p>${jam.title}</p>`,
         latlng: new kakao.maps.LatLng(jam.latitude, jam.longitude),
       });
     });
+    const map = getMap();
     for (let i = 0; i < mapPoints.length; i += 1) {
       // 마커를 생성합니다
       const marker = new kakao.maps.Marker({
@@ -103,16 +107,13 @@ const Map = ({ jamData }) => {
     }
   };
 
-  useEffect(() => {
-    console.log(currentLocation);
-    // getMap();
-    MapPin();
-  }, [currentLocation]);
+  // useEffect(() => {
+  //   getMap();
+  // }, [currentLocation]);
 
-  useState(() => {
-    console.log('currentCoordinate', currentCoordinate);
+  useEffect(() => {
     MapPin();
-  }, [currentCoordinate]);
+  }, [latitude, longitude]);
 
   return (
     <div
